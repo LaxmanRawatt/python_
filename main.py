25911537d18516d2d7bef95693c50f02b1c2f42a
LaxmanRawatt <lr409305@gmail.com>
2024-04-03 13:02:43 +0530

    djaihdal

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-printf("Hello Wolrd!")
-printf("hello laxman")
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+number = 5
+print("Factorial of", number, "is", factorial(number))
+
